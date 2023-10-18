644fde7a49b6800934534a902ed38710b357c444
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-10-17 19:02:55 -0600

    feat: algoritmo correcto de coloración
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- tarea5/coloracion.py
+++ tarea5/coloracion.py
@@ -1,71 +1,72 @@
 from typing import List, Tuple
 
 Intervalo = Tuple[int, int]
-Coloracion = Tuple[Intervalo, int]
+Coloracion = Tuple[int, Intervalo]
 
 
 def imprimir_intervalos(C: List[Coloracion]):
-    range_max = max([e for (_, e), _ in C])
-    range_min = min([b for (b, _), _ in C])
+    range_max = max([e for _, (_, e) in C])
+    range_min = min([b for _, (b, _) in C])
     print("|", end="")
     for i in range(range_min, range_max):
-        print(f"-{i:2d}-|", end="")
+        print(f"{i:2d}  |", end="")
     print('')
-
-    for (b, e), c in C:
-        print(" "*(b-range_min)*5, end="")
-        print("|", end='')
-        print(f"{c}"*(e-b-1)*5, end='')
-        print(f"{c}"*4, end="")
-        print("|")
-    print()
+    colores = max([c for c, _ in C])
+    for color in range(colores+1):
+        ini = range_min
+        for (b, e) in [(b, e) for c, (b, e) in C if c == color]:
+            print(" "*(b-ini)*(5 if ini == range_min else 4), end="")
+            print("|", end='')
+            print(f"{color}"*(e-b-1)*5, end='')
+            print(f"{color}"*4, end="")
+            print("|", end="")
+            ini = e
+        print()
 
 
 def coloracion_intervalo(I: List[Intervalo]) -> Tuple[int, List[Coloracion]]:
-    n: int = len(I)
-    if n == 0:
+    if I == []:
         return 0, []
     I = sorted(I, key=lambda i: i[1])  # O(n log n)
-    max_color: int = 0
-    color: int = 0
-    colores_fin: List[float] = [0]*n
-    coloracion: List[Coloracion] = [(I[0], 0)]
-    colores_fin[color] = I[0][1]
-
-    for (b, e) in I[1:]:
-        if b > colores_fin[color]:
-            coloracion.append(((b, e), color))
-            colores_fin[color] = e
-            if color == max_color:
-                color = 0
-            else:
-                color += 1
+    colores: int = 1
+    coloracion: List[Coloracion] = [(0, I[0])]
+    puntero: int = 0
+    for b, e in I[1:]:
+        color, (_, ec) = coloracion[puntero]
+        if b > ec:
+            coloracion.append((color, (b, e)))
+            puntero += 1
         else:
-            max_color += 1
-            coloracion.append(((b, e), max_color))
-            colores_fin[max_color] = e
-    return max_color+1, coloracion
+            coloracion.append((colores, (b, e)))
+            colores += 1
+    return colores, coloracion
 
 
 def contar_coloracion(I: List[Tuple[float, float]]):
-    if I == []:
+    n = len(I)
+    if n == 0:
         return 0
     I = sorted(I, key=lambda i: i[1])
-    color = 0
-    descarte = [I[0][1]]
-    for b, e in I[1:]:
-        if b > descarte[color]:
-            descarte[color] = e
-            color = (color+1) % len(descarte)
+    colores = 1
+    coloracion = [0]*n
+    puntero = 0
+    for i, (b, _) in enumerate(I[1:]):
+        _, descarte = I[puntero]
+        if b > descarte:
+            coloracion[i+1] = coloracion[puntero]
+            puntero += 1
         else:
-            descarte.append(e)
-    return len(descarte)
+            coloracion[i+1] = colores
+            colores += 1
+    return colores
 
 
 def main():
     I: List[Intervalo] = [
         (1, 3), (4, 6), (2, 5), (0, 8), (9, 11)
     ]
+    I = [(0, 6), (1, 3), (4, 5), (2, 7), (8, 9)]
+    I = [(0, 2), (1, 3), (2, 4), (3, 6), (2, 7), (4, 8), (5, 9), (7, 10)]
     num_etiquetas, coloracion = coloracion_intervalo(I)
     print(f"Colores: {contar_coloracion(I)}")
     print(f"Coloración: {coloracion}")
